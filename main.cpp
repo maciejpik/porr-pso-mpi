40bfbb22e8865ff7b4b577954294fc591555abdb
Maciej Pikulinski <mpikulinski@meil.pw.edu.pl>
2021-01-06 02:58:11 +0100

    PSO MPI prototype

--- main.cpp
+++ main.cpp
@@ -28,11 +28,64 @@ int main(int argc, char *argv[])
     int localParticlesNumber = getLocalParticlesNumber(processRank, numberOfProcesses, particlesNumber);
     printf("[%d] I will compute %d particles\n", processRank, localParticlesNumber);
 
-    for(int i = 0; i < localParticlesNumber; i++)
+    // for(int i = 0; i < localParticlesNumber; i++)
+    // {
+    //     printf("[%d] My local particle %3d has global Id %3d\n", processRank, i,
+    //         getGlobalParticleId(processRank, numberOfProcesses, particlesNumber, i));
+    // }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    bool stop = false;
+    int iteration = 1;
+    while (!stop)
     {
-        printf("[%d] My local particle %3d has global Id %3d\n", processRank, i,
-            getGlobalParticleId(processRank, numberOfProcesses, particlesNumber, i));
+        // Compute new posistions (S)
+        double *localBestPosition = new double[dimensions];
+        localBestPosition[0] = processRank;
+        for (int i = 1; i < dimensions; i++)
+            localBestPosition[i] = 0;
+        // Compute new postisions (E)
+
+        // Receive local best postisions (S)
+        double *receivedBestPositions = NULL;
+        if (processRank == ROOT)
+            receivedBestPositions = new double[numberOfProcesses * dimensions];
+
+        MPI_Gather((void *)localBestPosition, dimensions, MPI_DOUBLE,
+                   (void *)receivedBestPositions, dimensions, MPI_DOUBLE,
+                   ROOT, MPI_COMM_WORLD);
+        // Receive local best postisions (E)
+
+        // Find global best position
+        double *computedGlobalBestPosition = new double[dimensions];
+        if (processRank == ROOT)
+        {
+            for (int i = 0; i < dimensions; i++)
+                computedGlobalBestPosition[i] = -1;
+        }
+
+        // Broadcast global best position
+        MPI_Bcast((void *)computedGlobalBestPosition, dimensions, MPI_DOUBLE, ROOT,
+                  MPI_COMM_WORLD);
+
+        // Update best position
+        //printf("[%d] New best position [0] = %lf\n", processRank, computedGlobalBestPosition[0]);
+
+        // Compute stop criterion
+        if(iteration == 3)
+            stop = true;
+
+        iteration++;
+
+        // Clean just to allocate again in next iteration
+        delete localBestPosition, receivedBestPositions, computedGlobalBestPosition;
+        MPI_Barrier(MPI_COMM_WORLD);
     }
+
+    iteration--;
+    if(processRank == ROOT)
+        printf("[%d] Found solution after %d iterations\n", processRank, iteration);
 
     MPI_Finalize();
 
